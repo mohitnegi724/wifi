b95b67bb4b2dde3dfa840d425af71c9c0db63d23
mohitnegi724 <mohitnegi724@gmail.com>
2018-08-12 22:57:15 +0530

    first commit

---
+++ public/Javascript/Index.js
@@ -0,0 +1,7 @@
+let About = document.getElementsByClassName("About");
+let AboutValue = About.offset().top;
+// let AboutValue = ;
+// let PriceValue = ;
+// let ContactValue = ;
+
+alert(AboutValue)
